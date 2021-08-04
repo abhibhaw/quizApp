3364d313b4383fb99846d9e0008683e001d7fec3
Divyanshu <divyanshujha02@yahoo.com>
2021-08-05 03:25:12 +0530

    Added QuestionActivity

---
+++ app/src/main/java/com/divyanshu/quizapp/quizQuestionActivity.kt
@@ -0,0 +1,11 @@
+package com.divyanshu.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class quizQuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_question)
+    }
+}
